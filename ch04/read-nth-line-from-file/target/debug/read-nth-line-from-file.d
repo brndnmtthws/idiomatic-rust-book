5b5ac26c0d42a840ad993355f8c68b08b367bc5f
Brenden Matthews <github@brenden.brndn.io>
2024-04-13 13:30:44 -0400

    Add basic CI
    Also removed inadvertantly checked in target/ dir and fixed some minor
    lints.

--- ch04/read-nth-line-from-file/target/debug/read-nth-line-from-file.d
+++
@@ -1,1 +0,0 @@
-/Users/brenden/dev/design-patterns/code/ch04/read-nth-line-from-file/target/debug/read-nth-line-from-file: /Users/brenden/dev/design-patterns/code/ch04/read-nth-line-from-file/src/main.rs
