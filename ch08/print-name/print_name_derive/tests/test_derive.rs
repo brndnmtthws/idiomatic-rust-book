c530d826e91fcf1e2f8bdf07a875b08d21f23858
Lev Veyde <lveyde@gmail.com>
2024-04-23 22:06:45 +0300

    Chapter 8 updates
    Updated print-name:
    - Updated macro tests to test generic structs cases.
    
    Signed-off-by: Lev Veyde <lveyde@gmail.com>

--- ch08/print-name/print_name_derive/tests/test_derive.rs
+++ ch08/print-name/print_name_derive/tests/test_derive.rs
@@ -1,11 +1,21 @@
 use print_name::PrintName;
 use print_name_derive::PrintName;
+use std::marker::PhantomData;
 
 #[test]
 fn test_derive() {
     #[derive(PrintName)]
     struct MyStruct;
 
+    #[derive(PrintName)]
+    struct MyGenericStruct<T> {
+        phantom: PhantomData<T>,
+    }
+
     assert_eq!(MyStruct::name(), "MyStruct");
     MyStruct::print_name();
+    assert_eq!(MyGenericStruct::<i64>::name(), "MyGenericStruct");
+    assert_eq!(MyGenericStruct::<i128>::name(), "MyGenericStruct");
+    assert_eq!(MyGenericStruct::<String>::name(), "MyGenericStruct");
+    MyGenericStruct::<i128>::print_name();
 }
