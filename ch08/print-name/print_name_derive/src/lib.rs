b76ac7cbaf85ceb1ece79733fbc1b3f2f2e0034e
Lev Veyde <lveyde@gmail.com>
2024-04-23 21:51:02 +0300

    Chapter 8 updates
    Updated print-name:
    - Removed trait bounds from the macro, as it doesn't make sense in
      this case, as it did in the original example in syn.
    
    Signed-off-by: Lev Veyde <lveyde@gmail.com>

--- ch08/print-name/print_name_derive/src/lib.rs
+++ ch08/print-name/print_name_derive/src/lib.rs
@@ -2,17 +2,14 @@
 // https://github.com/dtolnay/syn/tree/master/examples/heapsize.
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{
-    parse_macro_input, parse_quote, DeriveInput, GenericParam, Generics,
-};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(PrintName)]
 pub fn print_name(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let generics = add_trait_bounds(input.generics);
     let (impl_generics, type_generics, where_clause) =
-        generics.split_for_impl();
+        input.generics.split_for_impl();
 
     let name = input.ident;
 
@@ -25,13 +22,4 @@ pub fn print_name(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
-}
-
-fn add_trait_bounds(mut generics: Generics) -> Generics {
-    for param in &mut generics.params {
-        if let GenericParam::Type(ref mut type_param) = *param {
-            type_param.bounds.push(parse_quote!(print_name::PrintName));
-        }
-    }
-    generics
 }
