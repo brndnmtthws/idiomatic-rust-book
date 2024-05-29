1656eec54ebd6becbef8a2b601e8b59202b99deb
Brenden Matthews <github@brenden.brndn.io>
2024-05-29 09:06:52 -0400

    Add ch04 example

--- ch04/pass-by-value/src/main.rs
+++ ch04/pass-by-value/src/main.rs
@@ -4,6 +4,14 @@ fn reverse(s: String) -> String {
     String::from_iter(v.iter())
 }
 
+fn reverse_and_uppercase(s: String) -> (String, String) {
+    let mut v = Vec::from_iter(s.chars());
+    v.reverse();
+    let reversed = String::from_iter(v.iter());
+    let uppercased = reversed.to_uppercase();
+    (reversed, uppercased)
+}
+
 fn main() {
     let s = String::from("😜🤖🪼");
     println!("Forward: {s}");
@@ -19,5 +27,17 @@ mod tests {
     fn test_reverse() {
         assert_eq!("rotator", reverse(String::from("rotator")));
         assert_eq!("abcdefg", reverse(String::from("gfedcba")));
+    }
+
+    #[test]
+    fn test_reverse_and_uppercase() {
+        assert_eq!(
+            reverse_and_uppercase("rotator".to_string()),
+            ("rotator".to_string(), "ROTATOR".to_string()),
+        );
+        assert_eq!(
+            reverse_and_uppercase("abcdefg".to_string()),
+            ("gfedcba".to_string(), "GFEDCBA".to_string()),
+        );
     }
 }
